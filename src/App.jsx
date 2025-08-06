4dcca943f993ac611570f885593b271ef2ac17e7
bindushishir1 <bindushishir004@gmail.com>
2025-08-06 23:34:40 +0545

    refector: code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './pages/Home'
+import CustomerProfile from './pages/CustomerProfile'
 
 const App = () => {
   return (
     <>
-    <Home/>
+    {/* <Home/> */}
+    <CustomerProfile/>
     </>
   )
 }
